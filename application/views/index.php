3bfe9718536979a7918c0866affce9951e8803a3
Yutisio12 <agungridhoyutisio@gmail.com>
2021-05-22 14:46:50 +0700

    x

---
+++ application/views/index.php
@@ -0,0 +1,7 @@
+<?php $this->load->view("_partials/head.php") ?>
+<?php $this->load->view("_partials/sidebar.php") ?>
+<?php $this->load->view("_partials/top.php") ?>
+
+<?php $this->load->view("_partials/subview.php") ?>
+
+<?php $this->load->view("_partials/foot.php") ?>
