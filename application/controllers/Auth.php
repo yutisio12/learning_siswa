b7ec72b4c0feb3ac10396d7c76564c5ea5873c3b
6un9 <agungridhoyutisio@gmail.com>
2021-05-27 22:15:40 +0700

    Update:
    - add detail kelas to cookies_kelas

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -33,11 +33,21 @@ class Auth extends CI_Controller {
 
         if($c_pass==$i_pass && isset($_POST['username'])){
 
-            $permission = $datadb['id'].';'.$datadb['name'].';'.$_POST['username'].';'.$datadb['role'];
+            $permission = $datadb['id'].';'.$datadb['name'].';'.$_POST['username'].';'.$datadb['role'].';'.$datadb['nip'];
 
             $permissions = json_encode($permission);
             setcookie('user', null, -1, '/'); 
             setcookie('user', $permissions, time() + (60 * 60 * 24 * 5), '/');
+
+            if($datadb['role']==1){
+                // guru soon, nunggu table dr ardi
+            } elseif($datadb['role']==2){
+                $where_detail['nip_siswa'] = $datadb['nip'];
+                $detail = $this->auth_model->find_siswa($where_detail)[0];
+                setcookie('cookies_kelas', null, -1, '/'); 
+                setcookie('cookies_kelas', $detail['kelas_siswa'], time() + (60 * 60 * 24 * 5), '/');
+            }
+
             $cookie = get_cookie('user');
             if($datadb['role']==0){
                 redirect('home');
@@ -49,12 +59,15 @@ class Auth extends CI_Controller {
             
         } else {
             delete_cookie('user');
+            setcookie('user', null, -1, '/'); 
+            setcookie('cookies_kelas', null, -1, '/'); 
             redirect('auth/index');
         }
     }
 
     public function logout(){
         setcookie('user', null, -1, '/'); 
+        setcookie('cookies_kelas', null, -1, '/'); 
         delete_cookie('user');
         redirect('auth/index');
     }
